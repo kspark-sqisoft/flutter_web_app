2ba49e8af136fc161728301012a20c16b078b627
박기순 <ks.park@sqisoft.com>
2025-09-04 09:15:03 +0900

    count 만들기 완료

---
+++ lib/counter.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Count:$count'),
+        ElevatedButton.icon(
+          onPressed: () {
+            setState(() {
+              count = count + 1;
+            });
+          },
+          label: Text('증가'),
+          icon: Icon(Icons.add),
+        ),
+      ],
+    );
+  }
+}
